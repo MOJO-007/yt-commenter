b16c78294fa75ea571bec3a0c9a8162c4c462e5b
MOJO <abhisatish75@gmail.com>
2023-08-18 23:09:40 +0530

    Modular code

--- authenticate.py
+++ authenticate.py
@@ -5,19 +5,40 @@ from google.oauth2.credentials import Credentials
 import requests
 import re
 
-# Replace with your own credentials
-CLIENT_ID = '782925300094-s0nnqlhsgl81t771uopqh770eku3e79d.apps.googleusercontent.com'
-CLIENT_SECRET = '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'
-SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']  # Adjust scope as needed
+def youtube_bot():
+    print("You are using youtube Bot:")
+    auth_token = get_auth_token()
+    video_id=get_video_id()
+    comment_text=input("Enter the comment to be added: ")
+    url = f'https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&access_token={auth_token}'
+    comment_data = {
+    "snippet": {
+        "videoId": video_id,
+        "topLevelComment": {
+            "snippet": {
+                "textOriginal": comment_text
+            }
+        }
+    }
+    }
+    response = requests.post(url, json=comment_data)
 
-# Create the flow
-flow = InstalledAppFlow.from_client_secrets_file(
-    './credentials.json',  # Path to your client secret JSON file
-    scopes=SCOPES
-)
+    if response.status_code == 200:
+        print("Comment added successfully.")
+    else:
+        print("Error adding comment.")
+        print("Response:", response.text)
+
+
+
+    return 0
 
 def get_auth_token():
-    # Run the authorization flow
+    SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
+    flow = InstalledAppFlow.from_client_secrets_file(
+    './credentials.json',  # Path to your client secret JSON file
+    scopes=SCOPES
+    )
     credentials = flow.run_local_server()
     return (credentials.token)
     
@@ -30,28 +51,6 @@ def get_video_id():
     else:
         raise Exception("Invalid YouTube link.") 
 
-# You can now use `credentials` to make authenticated requests to the YouTube Data API.
-
 if __name__ == "__main__":
-  auth_token = get_auth_token()
-  print(auth_token)
-  video_id=get_video_id()
-  url = f'https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&access_token={auth_token}'
-  comment_data = {
-    "snippet": {
-        "videoId": video_id,
-        "topLevelComment": {
-            "snippet": {
-                "textOriginal": "This is a test comment."
-            }
-        }
-    }
-}
-  response = requests.post(url, json=comment_data)
-
-# Check the response
-if response.status_code == 200:
-    print("Comment added successfully.")
-else:
-    print("Error adding comment.")
-    print("Response:", response.text)
+    youtube_bot()
+  
